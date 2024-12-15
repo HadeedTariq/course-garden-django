cd9fac6ebe6c8758a399afddf791f19de89ab572
Muhammad Hadeed Tariq <120406067+HadeedTariq@users.noreply.github.com>
2024-12-15 20:55:24 +0500

    create groups for each course now feedback only send to specific course

--- course_arden/student/consumers.py
+++ course_arden/student/consumers.py
@@ -1,6 +1,9 @@
+from venv import create
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 import logging
+
+from teacher.models import Feedback
 
 
 logger = logging.getLogger("django")
@@ -8,7 +11,6 @@ logger = logging.getLogger("django")
 
 class FeedbackConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-
         logger.info(f"WebSocket connection from {self.scope['client']}")
         self.course_id = self.scope["url_route"]["kwargs"]["course_id"]
         self.group_name = f"course_{self.course_id}"
@@ -21,14 +23,22 @@ class FeedbackConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                "type": "feedback_message",
-                "message": message,
-            },
-        )
+        text_data_type  = text_data_json["message_type"]
+        if text_data_type == 'reply':
+            return "dfd"
+        else:
+            feedback=Feedback.objects.create(content=message,user_id='',course_id=self.course_id)
+            feedback=feedback.save()
+            await self.channel_layer.group_send(
+                self.group_name,
+                {
+                    "type": "feedback_message",
+                    "message": message,
+                    "feedback_id": feedback['id'],
+                },
+            )
 
-    async def broadcast_message(self, event):
+    async def feedback_message(self, event):
         message = event["message"]
-        await self.send(text_data=json.dumps({"message": message}))
+        feedback_id = event["feedback_id"]
+        await self.send(text_data=json.dumps({"message": message,'feedback_id':feedback_id}))
