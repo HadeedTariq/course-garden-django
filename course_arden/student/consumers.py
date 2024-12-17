5da52e211b05cecd86f98fc8f41c1d1a77e131f8
Muhammad Hadeed Tariq <120406067+HadeedTariq@users.noreply.github.com>
2024-12-17 14:55:51 +0500

    create feedback deletion functionality

--- course_arden/student/consumers.py
+++ course_arden/student/consumers.py
@@ -2,7 +2,6 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 import logging
 from channels.db import database_sync_to_async
-
 from teacher.models import Feedback, Reply
 
 logger = logging.getLogger("django")
@@ -20,23 +19,20 @@ class FeedbackConsumer(AsyncWebsocketConsumer):
         pass
 
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        text_data_type = text_data_json["message_type"]
-        user_id = text_data_json["user_id"]
+        data = json.loads(text_data)
+        message_type = data.get("message_type")
 
-        if text_data_type == "reply":
-            await self.handle_reply(message, user_id, text_data_json["feedback_id"])
-        elif text_data_type == "feedback":
-            await self.handle_feedback(message, user_id)
-        elif text_data_type == "delete_feedback":
-            await self.handle_delete_feedback(
-                message, user_id, text_data_json["feedback_id"]
-            )
-        elif text_data_type == "delete_reply":
-            await self.handle_delete_reply(message, user_id)
+        # Dispatch to specific handler based on message type
+        handler = getattr(self, f"handle_{message_type}", None)
+        if handler:
+            await handler(data)
+        else:
+            logger.warning(f"Unhandled message type: {message_type}")
 
-    async def handle_feedback(self, message, user_id):
+    async def handle_feedback(self, data):
+        message = data["message"]
+        user_id = data["user_id"]
+
         feedback = await database_sync_to_async(Feedback.objects.create)(
             content=message,
             user_id=user_id,
@@ -54,21 +50,22 @@ class FeedbackConsumer(AsyncWebsocketConsumer):
         )
 
     async def feedback_message(self, event):
-        message = event["message"]
-        feedback_id = event["feedback_id"]
-        user_id = event["user_id"]
         await self.send(
             text_data=json.dumps(
                 {
                     "message_type": "feedback",
-                    "message": message,
-                    "feedback_id": feedback_id,
-                    "user_id": user_id,
+                    "message": event["message"],
+                    "feedback_id": event["feedback_id"],
+                    "user_id": event["user_id"],
                 }
             )
         )
 
-    async def handle_reply(self, message, user_id, feedback_id):
+    async def handle_reply(self, data):
+        message = data["message"]
+        user_id = data["user_id"]
+        feedback_id = data["feedback_id"]
+
         feedback_reply = await database_sync_to_async(Reply.objects.create)(
             content=message,
             user_id=user_id,
@@ -87,24 +84,66 @@ class FeedbackConsumer(AsyncWebsocketConsumer):
         )
 
     async def feedback_reply(self, event):
-        message = event["message"]
-        feedback_id = event["feedback_id"]
-        user_id = event["user_id"]
-        reply_id = event["reply_id"]
         await self.send(
             text_data=json.dumps(
                 {
                     "message_type": "reply",
-                    "message": message,
-                    "feedback_id": feedback_id,
-                    "user_id": user_id,
-                    "reply_id": reply_id,
+                    "message": event["message"],
+                    "feedback_id": event["feedback_id"],
+                    "user_id": event["user_id"],
+                    "reply_id": event["reply_id"],
                 }
             )
         )
 
-    async def handle_delete_feedback(self, event):
-        return
+    async def handle_delete_feedback(self, data):
+        feedback_id = data["feedback_id"]
+        user_id = data["user_id"]
+        await database_sync_to_async(
+            Feedback.objects.filter(id=feedback_id, user_id=user_id).delete
+        )()
+        logger.info(f"Feedback {feedback_id} deleted by user {user_id}")
 
-    async def handle_delete_reply(self, event):
-        return
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": "feedback_deleted",
+                "feedback_id": feedback_id,
+            },
+        )
+
+    async def feedback_deleted(self, event):
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "message_type": "delete_feedback",
+                    "feedback_id": event["feedback_id"],
+                }
+            )
+        )
+
+    async def handle_delete_reply(self, data):
+        reply_id = data["reply_id"]
+        user_id = data["user_id"]
+        await database_sync_to_async(
+            Reply.objects.filter(id=reply_id, user_id=user_id).delete
+        )()
+        logger.info(f"Reply {reply_id} deleted by user {user_id}")
+
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": "reply_deleted",
+                "reply_id": reply_id,
+            },
+        )
+
+    async def reply_deleted(self, event):
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "message_type": "delete_reply",
+                    "reply_id": event["reply_id"],
+                }
+            )
+        )
